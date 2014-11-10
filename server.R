f4d5548f287f356543d14b1c8f56c1b789df06dc
santiagombv <santiagombv@gmail.com>
2014-11-09 21:20:55 -0300

    suppress warnings. Fix the max number of combinations

--- server.R
+++ server.R
@@ -1,8 +1,15 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
 library(shiny)
 
 shinyServer(function(input, output) {
-  output$distPlot <- renderPlot({ 
-    if(input$ale*input$genes < 31){  
+    output$distPlot <- renderPlot({ 
+      if(input$ale*input$genes < 31){  
       alelos <- seq(0,input$ale-1)
       lista<- list(input$genes)
       for(i in 1:input$genes) lista[[i]]<-alelos
@@ -10,9 +17,10 @@ shinyServer(function(input, output) {
       phenotype <- apply(tab, 1, sum)
       G <- ggplot(data=data.frame(phenotype), aes(x=phenotype)) + geom_histogram()
       G <- G + theme_bw()
-      G} else {
+      suppressMessages(print(G))} else {
         plot.new()
         text(0.5, 0.5, "demasiadas combinaciones")
       }
+      })
   })
-})
+
--- ui.R
+++ ui.R
@@ -1,19 +1,26 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
 library(shiny)
 library(ggplot2)
 
 shinyUI(fluidPage(
-  
+
   # Application title
   titlePanel("Rasgos cuantitativos"),
-  
+
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       sliderInput("genes", label = "genes", min=1, max=15, value =2, step = 1), 
       sliderInput("ale", label = "alelos", min=2, max=5, value =2, step = 1),
       width = 3),
-    
-    
+      
+
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("distPlot")
